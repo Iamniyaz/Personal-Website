34ee7dbae94d99b5799342c3aeed7dc27f2fd6bb
Niyaz <skniyazamd@gmail.com>
2025-04-20 18:25:48 +0530

    fixes

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,4 +1,3 @@
-import { animate } from "framer-motion";
 import { style } from "framer-motion/client";
 import {    PropsWithChildren } from "react"
 import { twMerge } from "tailwind-merge";
@@ -15,17 +14,16 @@ export const HeroOrbit = ({children,size,
     }>) =>{
     return (
         <div className='absolute left-1/2 top-1/2 -translate-x-1/2  -translate-y-1/2 -z-20 '>
-           <div className={twMerge(shouldSpin === true && "animate-spin")} style={{
-    animationDuration: spinDuration,
-}}>
-
+            <div className={twMerge(shouldOrbit ===true && "animate-spin")} style={{
+                animationDuration: orbitDuration,
+            }}>
         <div className=' flex items-start justify-start 
         '
          style={{
         transform:`rotate(${rotation}deg) `,
         height:`${size}px`,
         width:`${size}px`}}>
-            <div className={twMerge(shouldSpin===true && "animate-spin")}style={{
+            <div className={twMerge(shouldSpin===true && "animate-spin")} style={{
                 animationDuration: spinDuration,
             }}>
         <div className=' inline-flex ' style={{
@@ -35,5 +33,6 @@ export const HeroOrbit = ({children,size,
         </div>
         </div>
         </div>
-    )
+    
+    );
 }
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -21,41 +21,35 @@ export const HeroSection = () => {
         shouldSpin?:boolean;
         spinDuration?:string;
         orbitDuration?:string; */}
-        <HeroOrbit size={430} rotation={-20} shouldOrbit orbitDuration="30s" shouldSpin
-    spinDuration="3s">
+        <HeroOrbit size={430} rotation={-20} shouldOrbit orbitDuration="30s" shouldSpin spinDuration='3s'>
           <SparkleIcon className='size-8 text-emerald-300/20' />
         </HeroOrbit>
-    <HeroOrbit size={440} rotation={79}shouldOrbit orbitDuration="32s" shouldSpin
-    spinDuration="3s">
+    <HeroOrbit size={440} rotation={79}shouldOrbit orbitDuration="32s" shouldSpin spinDuration='3s'>
       <SparkleIcon className='size-5 text-emerald-300/20' />
     </HeroOrbit>
     <HeroOrbit size={520} rotation={-42}shouldOrbit orbitDuration="34s">
       <div className='size-2 rounded-full text-emerald-300/20' />
     </HeroOrbit>
-    <HeroOrbit size={530} rotation={179}shouldOrbit orbitDuration="36s"  shouldSpin
-    spinDuration="3s">
+    <HeroOrbit size={530} rotation={179}shouldOrbit orbitDuration="36s" shouldSpin spinDuration='6s'>
       <SparkleIcon className='size-10 text-emerald-300/20' />
     </HeroOrbit>
-    <HeroOrbit size={550} rotation={23}shouldOrbit orbitDuration="38s"  shouldSpin
-    spinDuration="6s">
+    <HeroOrbit size={550} rotation={23}shouldOrbit orbitDuration="38s" shouldSpin spinDuration='6s'>
       <StarIcon className='size-12 text-emerald-300' />
     </HeroOrbit>
     <HeroOrbit size={590} rotation={99}
-    shouldOrbit orbitDuration="40s"  shouldSpin
-    spinDuration="6s">
+    shouldOrbit orbitDuration="40s" shouldSpin spinDuration='6s'>
       <StarIcon className='size-8 text-emerald-300' />
     </HeroOrbit>
     <HeroOrbit size={650} rotation={-5}shouldOrbit orbitDuration="42s">
       <div className='size-2 rounded-full text-emerald-300/20' />
     </HeroOrbit>
-    <HeroOrbit size={710} rotation={143}shouldOrbit orbitDuration="44s">
+    <HeroOrbit size={710} rotation={143}shouldOrbit orbitDuration="44s" shouldSpin spinDuration='6s'>
       <SparkleIcon className='size-14 text-emerald-300/20' />
     </HeroOrbit>
     <HeroOrbit size={720} rotation={87}shouldOrbit orbitDuration="46s">
       <div className='size-3 rounded-full text-emerald-300/20' />
     </HeroOrbit>
-    <HeroOrbit size={800} rotation={-73}shouldOrbit orbitDuration="48s" shouldSpin
-    spinDuration="6s">
+    <HeroOrbit size={800} rotation={-73}shouldOrbit orbitDuration="48s" shouldSpin spinDuration='6s'>
     <StarIcon className="size-28 text-emerald-300" />
     </HeroOrbit>
     </div>
