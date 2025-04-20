c05b1ad2fe12e55ecea25b3aa78a337956b740b0
Niyaz <skniyazamd@gmail.com>
2025-04-20 16:57:16 +0530

    fixes

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -10,9 +10,10 @@ export const ContactSection = () => {
         }}> </div>
         <div className='flex flex-col gap-8 items-center md:flex-row md:gap-16  '>
           <div>
-    <h2 className='font-serif text-2xl md:text-3xl'>"Let's create something amazing together"</h2>
-    <p className='text-sm md:text-base mt-2'>
-     " Ready to bring your next project to life? Let's connect and discuss how I can help you achieve your goals." </p>
+          <h2 className='font-serif text-2xl md:text-3xl'>Let&apos;s create something amazing together</h2>
+<p className='text-sm md:text-base mt-2'>
+  Ready to bring your next project to life? Let&apos;s connect and discuss how I can help you achieve your goals.
+</p>
     <div>
     <button className='text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 mt-8 w-max border border-gray-950'>
       <span className='font-semibold'>Contact Me</span>
