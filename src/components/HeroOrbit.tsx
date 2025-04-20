c2fbeead9298964a23d9c4761e71cf4e048077fb
Niyaz <skniyazamd@gmail.com>
2025-04-20 18:13:38 +0530

    fixes

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -26,7 +26,7 @@ export const HeroOrbit = ({children,size,
         height:`${size}px`,
         width:`${size}px`}}>
             <div className={twMerge(shouldSpin===true && "animate-spin")}style={{
-                animateDuration: spinDuration,
+                animationDuration: spinDuration,
             }}>
         <div className=' inline-flex ' style={{
             transform:`rotate(${rotation * -1}deg)`,
